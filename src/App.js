31f91dc6e108b5b1f0ae25f7f5f2cd05af66eeb2
Mehedi Bhuiyan <mehedibuiyan734@gmail.com>
2021-07-14 02:57:05 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+    let [input , setInput] = useState('')
+    let [weather , setWeather] = useState({})
+
+    const api ={
+      key:"48070f77bfd407c99032b5fd6bb66111",
+      base:"https://api.openweathermap.org/data/2.5/"
+    }
+
+    const search = evt => {
+      if(evt.key==="Enter"){
+          fetch(`${api.base}weather?q=${input}&units=metric&appid=${api.key}`)
+          .then(res => res.json())
+          .then(data => {
+            setWeather(data)
+          })
+          console.log(weather);
+      }
+    }
+
+
+
+
+
+  
+ const  dateBuilder = (d) => {
+  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  let day = days[d.getDay()]
+  let month = months[d.getMonth()]
+  let date = d.getDate()
+  let year = d.getFullYear()
+
+  return ` ${day} ${date} ${month} ${year}`
+ }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="search-box">
+          <input 
+          type="text"
+           className="search-bar" 
+           placeholder="search..."
+           value={input}
+           onChange={e => setInput(e.target.value)}
+           onKeyPress={search}
+          />
+        </div>
+
+        <div className="location-box">
+          <div className="location">Dhaka,BD</div>
+          <div className="date">{dateBuilder (new Date())}</div>
+        </div>
+
+        <div className="weather-box">
+          <div className="temp">
+            15°C
+          </div>
+          <div className="weather">
+            Sunny
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
