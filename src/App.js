40a8d7aff78dac4fdec4f2e663586f806dc973a5
Mehedi Bhuiyan <mehedibuiyan734@gmail.com>
2021-07-14 17:34:50 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-    let [input , setInput] = useState('')
+    let [input , setInput] = useState("")
     let [weather , setWeather] = useState({})
 
     const api ={
@@ -41,7 +41,7 @@ function App() {
   return ` ${day} ${date} ${month} ${year}`
  }
   return (
-    <div className="App">
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp>16) ? 'app warm' : 'app') : 'app'}>
       <main>
         <div className="search-box">
           <input 
@@ -52,21 +52,25 @@ function App() {
            onChange={e => setInput(e.target.value)}
            onKeyPress={search}
           />
-        </div>
-
-        <div className="location-box">
-          <div className="location">Dhaka,BD</div>
+          <h1 className="title">FIND WEATHER OF YOUR CITY</h1>
+        </div> 
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className="location-box">
+          <div className="location">{weather.name} {((weather || {}).sys || {}).country}</div>
           <div className="date">{dateBuilder (new Date())}</div>
         </div>
 
         <div className="weather-box">
           <div className="temp">
-            15°C
+            {weather.main.temp}°C
           </div>
           <div className="weather">
-            Sunny
+          {weather.weather[0].main}
           </div>
         </div>
+          </div>
+        ):('')}
       </main>
     </div>
   );
